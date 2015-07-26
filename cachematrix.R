a422d2d95e4d455fd60a5471f8aa08def903de26
Eduardo Bonilla <ebonillabraddock@hotmail.com>
2015-07-26 21:16:26 +0200

    my code for this assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## For this assignment, build two function that play around enviroments. 
+## 1st function, makeCacheMatrix, obtains the inverse of a matrix putting the outputs in cache so it can be called from somewhere else.
+## 2nd function, cacheinverse, compares if the parameter matrix is the same of the one wich previously its inverse has been obtained
+## and kept in cache, if so directly returns de inverse if not then calculates thhe inverse.  
 
-## Write a short comment describing this function
+## Calculate the inverse of a matrix and keeps the results in cache
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<- function(x=matrix()){
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function()x
+        setinverse<-function(solve) m<<-solve
+        getinverse<-function() m
+        list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Checks if the matrix is the same, by the instruction isTRUE(identical(x,m)) and if not calculates its inverse.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheinverse<-function(x, ...){
+        m=x$getinverse()
+        if (isTRUE(identical(x,m))){
+                message ("getting cached data")
+                return(m)
+        }
+        
+        data<-x$get()
+        m<-solve(data, ...)
+        x$setinverse(m)
+        m
 }
